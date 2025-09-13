324577c6d678243a49bcfe49a4419504f41193c0
gantaro <gantaro1967@gmail.com>
2025-09-13 19:43:12 +0900

    28_@Email@Min追加

--- src/main/java/raisetech/studentmanagement/data/Student.java
+++ src/main/java/raisetech/studentmanagement/data/Student.java
@@ -1,5 +1,7 @@
 package raisetech.studentmanagement.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,8 +18,10 @@ public class Student {
   private String kanaName;
   private String nickName;
   @NotEmpty(message = "emailは空にできません。")
+  @Email(message = "有効なールアドレス形式で入力して下さい。")
   private String email;
   private String address;
+  @Min(value = 18, message = "登録は18以上になります。")
   private int age;
   private String gender;
   private String remark;
