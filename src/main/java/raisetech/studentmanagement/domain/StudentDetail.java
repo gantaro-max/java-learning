df7ad314b4a911e6cc84309c69842e2cf7fab754
gantaro <gantaro1967@gmail.com>
2025-09-13 18:38:42 +0900

    28_動作確認

--- src/main/java/raisetech/studentmanagement/domain/StudentDetail.java
+++ src/main/java/raisetech/studentmanagement/domain/StudentDetail.java
@@ -1,7 +1,6 @@
 package raisetech.studentmanagement.domain;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Getter;
@@ -16,8 +15,6 @@ public class StudentDetail {
   @Valid
   @NotNull(message = "受講生情報は必須です。")
   private Student student;
-  @Valid
-  @NotEmpty(message = "コース内容が作成出来ていません。")
   private List<StudentsCourses> studentsCourses;
   @Valid
   @NotNull(message = "コース選択は必須です。")
