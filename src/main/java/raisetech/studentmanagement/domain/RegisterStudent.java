52844988be44cdb20c0ca8b0d8e034dd102c6e7c
gantaro <gantaro1967@gmail.com>
2025-10-05 22:16:33 +0900

    OpenAPIでドキュメント記述

--- src/main/java/raisetech/studentmanagement/domain/RegisterStudent.java
+++ src/main/java/raisetech/studentmanagement/domain/RegisterStudent.java
@@ -1,5 +1,7 @@
 package raisetech.studentmanagement.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -8,24 +10,34 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生登録Request")
 @Getter
 @Setter
 public class RegisterStudent {
 
+  @Schema(description = "受講生名", requiredMode = RequiredMode.REQUIRED)
   @NotBlank(message = "名前は必須です")
   private String fullName;
+  @Schema(description = "受講生名（カナ）", requiredMode = RequiredMode.REQUIRED)
   @NotBlank(message = "カナ名は必須です")
   private String kanaName;
+  @Schema(description = "ニックネーム")
   private String nickName;
+  @Schema(description = "メールアドレス", requiredMode = RequiredMode.REQUIRED)
   @NotBlank(message = "emailは必須です")
   @Email(message = "有効なメールアドレス形式で入力して下さい")
   private String email;
+  @Schema(description = "地域")
   private String address;
+  @Schema(description = "年齢", requiredMode = RequiredMode.REQUIRED)
   @NotNull(message = "年齢は空にできません")
   @Min(value = 18, message = "登録は18以上になります")
   private Integer age;
+  @Schema(description = "性別")
   private String gender;
+  @Schema(description = "備考")
   private String remark;
+  @Schema(description = "コースID", requiredMode = RequiredMode.REQUIRED)
   @NotEmpty(message = "コースIDは空にできません")
   private String courseId;
 }
